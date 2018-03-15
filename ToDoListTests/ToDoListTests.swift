d799a8591391b61d15457c69e3e7f66666c90ed9
Victor C <victorcheng@Victors-MacBook-Pro.local>
2018-03-15 00:44:40 -0700

    Add Unit Test

--- ToDoListTests/ToDoListTests.swift
+++ ToDoListTests/ToDoListTests.swift
@@ -9,27 +9,29 @@
 import XCTest
 @testable import ToDoList
 
+var toDoItemTest: ToDoItem!
+
 class ToDoListTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        toDoItemTest = ToDoItem(title: "Milk")
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        
+        toDoItemTest = nil
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testTitleSet() {
+        XCTAssertEqual(toDoItemTest.title, "Milk")
     }
     
     func testPerformanceExample() {
-        // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
+            
         }
     }
     
